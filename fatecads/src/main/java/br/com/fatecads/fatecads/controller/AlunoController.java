335ea9c0e902ae7a7f12514952cc001e168597d8
KaykyOne <kaykyzioti@gmail.com>
2026-03-23 17:27:39 -0300

    adicao controller

--- fatecads/src/main/java/br/com/fatecads/fatecads/controller/AlunoController.java
+++ fatecads/src/main/java/br/com/fatecads/fatecads/controller/AlunoController.java
@@ -2,15 +2,34 @@ package br.com.fatecads.fatecads.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import br.com.fatecads.fatecads.entity.Aluno;
 import br.com.fatecads.fatecads.service.AlunoService;
+
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @RequestMapping("/alunos")
 public class AlunoController {
-    
+
     // Injeção de dependência do serviço de alunos
     @Autowired
     private AlunoService alunoService;
+
+    @PostMapping("/salvar")
+    public String salvar(@ModelAttribute Aluno aluno) {
+        alunoService.save(aluno);
+        return "redirect:/alunos";
+    }
+
+    @GetMapping("/listar")
+    public String listar(Model model) {
+        model.addAttribute("alunos", alunoService.findAll());
+        return "alunos/listar";
+    }
+    
 }
